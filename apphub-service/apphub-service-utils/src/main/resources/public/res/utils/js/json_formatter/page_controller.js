b9de1eee8c6d75648e5a12c9d4339a2b1b1edf61
bondabear@gmail.com <Saphy371321>
2021-06-18 23:40:35 +0200

    Right click move in Log and JSON formatter

--- apphub-service/apphub-service-utils/src/main/resources/public/res/utils/js/json_formatter/page_controller.js
+++ apphub-service/apphub-service-utils/src/main/resources/public/res/utils/js/json_formatter/page_controller.js
@@ -1,5 +1,6 @@
 (function PageController(){
     scriptLoader.loadScript("/res/utils/js/json_formatter/format_controller.js");
+    scriptLoader.loadScript("/res/common/js/animation/move_controller.js");
 
     window.ids = {
         input: "input",
@@ -12,5 +13,8 @@
 
     $(document).ready(function(){
         eventProcessor.processEvent(new Event(events.LOAD_LOCALIZATION, {module: "utils", fileName: "json_formatter"}));
+        addRightClickMove("result-container", "result");
+        document.getElementById("result-container")
+            .addEventListener('contextmenu', event => event.preventDefault());
     });
 })();
