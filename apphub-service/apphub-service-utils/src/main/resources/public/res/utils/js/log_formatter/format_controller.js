f997f690e87def9f95caaf4f46550d50f69ee9d9
bondabear@gmail.com <Saphy371321>
2021-06-18 15:37:18 +0200

    Look fancy

--- apphub-service/apphub-service-utils/src/main/resources/public/res/utils/js/log_formatter/format_controller.js
+++ apphub-service/apphub-service-utils/src/main/resources/public/res/utils/js/log_formatter/format_controller.js
@@ -142,11 +142,11 @@
 
         const processedRecords = processLogRecords();
 
-        const formatted = JSON.stringify(processedRecords, null, 2)
+        const formatted = syntaxHighlight(JSON.stringify(processedRecords, null, 4))
             .replaceAll("\\n", "<BR>")
             .replaceAll("\\r", "")
             .replaceAll("\\t", "    ");
-            resultContainer.innerText = formatted;
+        resultContainer.innerHTML = formatted;
 
         function processLogRecords(){
             const data = JSON.parse(JSON.stringify(logRecords));
@@ -179,6 +179,25 @@
                         function(parameter){return record[parameter]}
                     )
             }
+        }
+
+        function syntaxHighlight(json) {
+            json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+            return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+                var cls = 'number';
+                if (/^"/.test(match)) {
+                    if (/:$/.test(match)) {
+                        cls = 'key';
+                    } else {
+                        cls = 'string';
+                    }
+                } else if (/true|false/.test(match)) {
+                    cls = 'boolean';
+                } else if (/null/.test(match)) {
+                    cls = 'null';
+                }
+                return '<span class="' + cls + '">' + match + '</span>';
+            });
         }
     }
 
