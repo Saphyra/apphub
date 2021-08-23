4872e0680ee382e4b1c2c8a7ff64f58b73f326a5
bondabear@gmail.com <Saphy371321>
2021-08-23 18:39:27 +0200

    Rename player when characterName is changed

--- apphub-service/apphub-service-skyxplore/apphub-service-skyxplore-data/src/test/java/com/github/saphyra/apphub/service/skyxplore/data/character/service/creation/CharacterCreationServiceTest.java
+++ apphub-service/apphub-service-skyxplore/apphub-service-skyxplore-data/src/test/java/com/github/saphyra/apphub/service/skyxplore/data/character/service/creation/CharacterCreationServiceTest.java
@@ -1,14 +1,17 @@
 package com.github.saphyra.apphub.service.skyxplore.data.character.service.creation;
 
 import com.github.saphyra.apphub.api.skyxplore.model.SkyXploreCharacterModel;
+import com.github.saphyra.apphub.api.skyxplore.model.game.PlayerModel;
 import com.github.saphyra.apphub.service.skyxplore.data.character.dao.CharacterDao;
 import com.github.saphyra.apphub.service.skyxplore.data.character.dao.SkyXploreCharacter;
+import com.github.saphyra.apphub.service.skyxplore.data.save_game.dao.player.PlayerDao;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.Arrays;
 import java.util.UUID;
 
 import static org.mockito.BDDMockito.given;
@@ -17,6 +20,7 @@ import static org.mockito.Mockito.verify;
 @RunWith(MockitoJUnitRunner.class)
 public class CharacterCreationServiceTest {
     private static final UUID USER_ID = UUID.randomUUID();
+    private static final String PLAYER_NAME = "player-name";
 
     @Mock
     private CharacterDao characterDao;
@@ -27,21 +31,32 @@ public class CharacterCreationServiceTest {
     @Mock
     private CharacterModelToCharacterConverter converter;
 
+    @Mock
+    private PlayerDao playerDao;
+
     @InjectMocks
     private CharacterCreationService underTest;
 
     @Mock
     private SkyXploreCharacter character;
 
+    @Mock
+    private PlayerModel playerModel;
+
     @Test
     public void create() {
-        SkyXploreCharacterModel model = new SkyXploreCharacterModel();
+        SkyXploreCharacterModel model = SkyXploreCharacterModel.builder()
+            .name(PLAYER_NAME)
+            .build();
 
         given(converter.convert(USER_ID, model)).willReturn(character);
+        given(playerDao.getByUserId(USER_ID)).willReturn(Arrays.asList(playerModel));
 
         underTest.create(USER_ID, model);
 
         verify(validator).validate(USER_ID, model);
         verify(characterDao).save(character);
+        verify(playerModel).setUsername(PLAYER_NAME);
+        verify(playerDao).save(playerModel);
     }
 }
