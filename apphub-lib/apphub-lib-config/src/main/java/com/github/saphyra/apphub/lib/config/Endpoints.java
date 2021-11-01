d713caadbecf5e30349f21d0635ffe34e22b4d2a
bondabear@gmail.com <Saphy371321>
2021-11-01 23:32:24 +0100

    Training is a module group, books are modules

--- apphub-lib/apphub-lib-config/src/main/java/com/github/saphyra/apphub/lib/config/Endpoints.java
+++ apphub-lib/apphub-lib-config/src/main/java/com/github/saphyra/apphub/lib/config/Endpoints.java
@@ -17,7 +17,6 @@ public class Endpoints {
     public static final String UTILS_JSON_FORMATTER_PAGE = "/web/utils/json-formatter";
     public static final String UTILS_LOG_FORMATTER_PAGE = "/web/utils/log-formatter";
     public static final String UTILS_BASE64_ENCODER_PAGE = "/web/utils/base64";
-    public static final String TRAINING_INDEX_PAGE = "/web/training";
 
     //Training
     public static final String TRAINING_BOOK_PAGE = "/web/training/{book}/{chapter}";
--- apphub-service/apphub-service-training/src/main/java/com/github/saphyra/apphub/service/training/TrainingPageController.java
+++ apphub-service/apphub-service-training/src/main/java/com/github/saphyra/apphub/service/training/TrainingPageController.java
@@ -9,11 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 @Slf4j
 class TrainingPageController {
-    @GetMapping(Endpoints.TRAINING_INDEX_PAGE)
-    String index() {
-        return "index";
-    }
-
     @GetMapping(Endpoints.TRAINING_BOOK_PAGE)
     String book(@PathVariable("book") String book, @PathVariable("chapter") String chapter) {
         log.info("Querying chapter {} of book {}", chapter, book);
