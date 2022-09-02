ff82276cf5ac0b949d6a660fb77473400ab6a136
bondabear@gmail.com <Saphy371321>
2022-09-02 14:48:26 +0200

    Limit number of users can be searched at the same time

--- apphub-service/apphub-service-user/src/main/java/com/github/saphyra/apphub/service/user/data/dao/user/UserRepository.java
+++ apphub-service/apphub-service-user/src/main/java/com/github/saphyra/apphub/service/user/data/dao/user/UserRepository.java
@@ -1,5 +1,6 @@
 package com.github.saphyra.apphub.service.user.data.dao.user;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -13,7 +14,7 @@ public interface UserRepository extends CrudRepository<UserEntity, String> {
     Optional<UserEntity> findByUsername(String username);
 
     @Query("SELECT u from UserEntity u WHERE LOWER(u.username) LIKE CONCAT('%', LOWER(:queryString), '%') OR LOWER(u.email) LIKE CONCAT('%', LOWER(:queryString), '%')")
-    List<UserEntity> getByUsernameOrEmailContainingIgnoreCase(@Param("queryString") String queryString);
+    List<UserEntity> getByUsernameOrEmailContainingIgnoreCase(@Param("queryString") String queryString, Pageable pageable);
 
     @Query("SELECT u FROM UserEntity u WHERE u.markedForDeletion=true ORDER BY u.markedForDeletionAt nulls last")
     List<UserEntity> getByUsersMarkedToDelete();
