9a5e466266b4c9f6a17e089a674192be329e548b
bondabear@gmail.com <Saphy371321>
2023-06-07 19:41:02 +0200

    Modules - Remove from WebContent

--- apphub-service/apphub-service-platform/apphub-service-platform-web_content/src/main/resources/public/res/modules/js/modules/page_controller.js
+++
@@ -1,7 +0,0 @@
-scriptLoader.loadScript("/res/modules/js/modules/modules_controller.js");
-
-(function PageController(){
-    $(document).ready(function(){
-        localization.loadLocalization("modules", "modules");
-    });
-})();
