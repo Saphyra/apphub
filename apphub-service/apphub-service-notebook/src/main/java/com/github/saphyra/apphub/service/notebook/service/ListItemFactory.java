fe6b1daf21b044a611fda7e58395b38b4fcd6516
ahvillat <phamvillatoro@gmail.com>
2023-06-13 19:57:36 -0500

    ListItemCloneServiceTest Fix

--- apphub-service/apphub-service-notebook/src/main/java/com/github/saphyra/apphub/service/notebook/service/ListItemFactory.java
+++ apphub-service/apphub-service-notebook/src/main/java/com/github/saphyra/apphub/service/notebook/service/ListItemFactory.java
@@ -13,15 +13,19 @@ import java.util.UUID;
 public class ListItemFactory {
     private final IdGenerator idGenerator;
 
+    public ListItem create(UUID userId, String title, UUID parent, ListItemType type) {
+        return create(userId, title, parent, type, false, false);
+    }
+
     public ListItem create(UUID userId, String title, UUID parent, ListItemType type, boolean pinned, boolean archived) {
         return ListItem.builder()
-            .listItemId(idGenerator.randomUuid())
-            .userId(userId)
-            .parent(parent)
-            .type(type)
-            .title(title)
-            .pinned(pinned)
-            .archived(archived)
-            .build();
+                .listItemId(idGenerator.randomUuid())
+                .userId(userId)
+                .parent(parent)
+                .type(type)
+                .title(title)
+                .pinned(pinned)
+                .archived(archived)
+                .build();
     }
 }
