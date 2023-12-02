328e7fcb548e4d3ba905b04fab27234265155ada
bondabear@gmail.com <Saphy371321>
2023-12-02 12:12:55 +0100

    Notebook - Custom Table - Enhance testCase report logging

--- apphub-integration-server/src/main/java/com/github/saphyra/integration/server/api/test_case/TestCaseController.java
+++ apphub-integration-server/src/main/java/com/github/saphyra/integration/server/api/test_case/TestCaseController.java
@@ -2,6 +2,7 @@ package com.github.saphyra.integration.server.api.test_case;
 
 import com.github.saphyra.integration.server.api.OneParam;
 import com.github.saphyra.integration.server.api.test_case.request.ReportTestCaseRequest;
+import com.github.saphyra.integration.server.api.test_case.request.TestCaseRunRequest;
 import com.github.saphyra.integration.server.service.test_case_run.TestCaseRunService;
 import com.github.saphyra.integration.server.service.test_case_run.TestCaseRunTimeQueryService;
 import com.github.saphyra.integration.server.util.Endpoints;
@@ -14,18 +15,38 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 public class TestCaseController {
+    private static volatile int MAX_LENGTH = Integer.MIN_VALUE;
+
     private final TestCaseRunService testCaseRunService;
     private final TestCaseRunTimeQueryService testCaseRunTimeQueryService;
 
     @PutMapping(Endpoints.REPORT_TEST_CASE)
     void reportTestCase(@PathVariable("testRunId") UUID testRunId, @RequestBody ReportTestCaseRequest request) {
-        log.info("{}", request);
+        logRequest(request.getTestCaseRun());
         testCaseRunService.report(testRunId, request);
+    }
+
+    private synchronized void logRequest(TestCaseRunRequest request) {
+        String displayedName = request.getTestCaseId()
+            .replace("com.github.saphyra.apphub.integration.", "");
+        if (displayedName.length() > MAX_LENGTH) {
+            MAX_LENGTH = displayedName.length();
+        }
+
+        String suffix = Stream.generate(() -> " ")
+            .limit(Math.max(0, MAX_LENGTH - displayedName.length()))
+            .collect(Collectors.joining());
+
+        displayedName += suffix;
+
+        log.info("TestCase {} {} in {}ms", displayedName, request.getStatus(), request.getDuration());
     }
 
     @PostMapping(Endpoints.GET_AVERAGE_RUN_TIME)
