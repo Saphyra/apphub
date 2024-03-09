f490c4141ba69df0ed7f99909c657a9c6a1fb9ad
saphyra <bondabear@gmail.com>
2024-03-09 18:17:23 +0100

    Use host's database

--- infra/deployment/script/release_port.sh
+++ infra/deployment/script/release_port.sh
@@ -1,4 +1,7 @@
 PORT=$1
 
-IFS=' ' read -r -a mgwarr <<< "$(netstat -aon | findstr $PORT)"
-taskkill //PID "$(sed -e 's/^[[:space:]]*//' <<<"${mgwarr[4]}")" //f
+if [[ $PORT != 5432 ]]; then
+  echo "Releasing port $PORT"
+  IFS=' ' read -r -a mgwarr <<< "$(netstat -aon | findstr $PORT)"
+  taskkill //PID "$(sed -e 's/^[[:space:]]*//' <<<"${mgwarr[4]}")" //f
+fi
--- port_forward.sh
+++ port_forward.sh
@@ -4,8 +4,10 @@ DATABASE_PORT=${3:-9002}
 
 echo "Forwarding to namespace $NAMESPACE with serverPort $SERVER_PORT and databasePort $DATABASE_PORT"
 
-./infra/deployment/script/release_port.sh $SERVER_PORT
-./infra/deployment/script/release_port.sh $DATABASE_PORT
+if [[ "$DATABASE_PORT" != 5432 ]]; then
+  ./infra/deployment/script/release_port.sh $DATABASE_PORT
+  kubectl port-forward deployment/postgres $DATABASE_PORT:5432 -n "$NAMESPACE" &
+fi
 
+./infra/deployment/script/release_port.sh $SERVER_PORT
 kubectl port-forward deployment/main-gateway $SERVER_PORT:8080 -n "$NAMESPACE" &
-kubectl port-forward deployment/postgres $DATABASE_PORT:5432 -n "$NAMESPACE" &
