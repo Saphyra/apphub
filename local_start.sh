c28861e9714a0806f7e520aed1be53005e23ccd8
bondabear@gmail.com <Saphy371321>
2024-05-13 13:48:16 +0200

    Platform - Local start script starts services in batches to decrease load

--- local_start.sh
+++ local_start.sh
@@ -13,11 +13,6 @@ if [ "$1" != "skipBuild" ]; then
   fi
 fi
 
-function waitStartup(){
-  echo "Pinging $1"
-  curl -s -o nul --head -X GET --silent --retry 20 --retry-connrefused --retry-delay 1 localhost:$1/platform/health
-}
-
 ./infra/deployment/script/local_start_app.sh 8081 "./apphub-service/apphub-service-platform/apphub-service-platform-event_gateway/target/application.jar"
 waitStartup 8081
 
@@ -26,12 +21,15 @@ waitStartup 8081
 ./infra/deployment/script/local_start_app.sh 8099 "./apphub-service/apphub-service-platform/apphub-service-platform-storage/target/application.jar" &
 ./infra/deployment/script/local_start_app.sh 8082 "./apphub-service/apphub-service-platform/apphub-service-platform-scheduler/target/application.jar" &
 ./infra/deployment/script/local_start_app.sh 8098 "./apphub-service/apphub-service-platform/apphub-service-platform-encryption/target/application.jar" &
+wait
 
 ./infra/deployment/script/local_start_app.sh 8084 "./apphub-service/apphub-service-custom/apphub-service-custom-villany_atesz/target/application.jar" &
+wait
 
 ./infra/deployment/script/local_start_app.sh 8091 "./apphub-service/apphub-service-skyxplore/apphub-service-skyxplore-data/target/application.jar" &
 ./infra/deployment/script/local_start_app.sh 8095 "./apphub-service/apphub-service-skyxplore/apphub-service-skyxplore-game/target/application.jar" &
 ./infra/deployment/script/local_start_app.sh 8093 "./apphub-service/apphub-service-skyxplore/apphub-service-skyxplore-lobby/target/application.jar" &
+wait
 
 ./infra/deployment/script/local_start_app.sh 8089 "./apphub-service/apphub-service-admin_panel/target/application.jar" &
 ./infra/deployment/script/local_start_app.sh 8100 "./apphub-service/apphub-service-utils/target/application.jar" &
@@ -41,16 +39,4 @@ waitStartup 8081
 ./infra/deployment/script/local_start_app.sh 8088 "./apphub-service/apphub-service-modules/target/application.jar" &
 ./infra/deployment/script/local_start_app.sh 8096 "./apphub-service/apphub-service-community/target/application.jar" &
 ./infra/deployment/script/local_start_app.sh 8097 "./apphub-service/apphub-service-calendar/target/application.jar" &
-
-sleep 20
-
-while IFS="" read -r service_data || [ -n "$service_data" ]
-do
-  IFS=' ' read -r -a array <<< "$service_data"
-  port=${array[3]}
-  TRIMMED="$(sed -e 's/[[:space:]]*$//' <<<"${port}")"
-
-  if [[ $port != "null" ]]; then
-    waitStartup $TRIMMED
-  fi
-done < ./infra/deployment/service/service_list
+wait
