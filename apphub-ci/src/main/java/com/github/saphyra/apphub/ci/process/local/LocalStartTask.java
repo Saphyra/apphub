8b8364966d5c688190a647bd1b163ca2ef74a9ee
bondabear@gmail.com <Saphy371321>
2024-08-30 13:41:04 +0200

    ApphubCI - Display startup time of services

--- apphub-ci/src/main/java/com/github/saphyra/apphub/ci/process/local/LocalStartTask.java
+++ apphub-ci/src/main/java/com/github/saphyra/apphub/ci/process/local/LocalStartTask.java
@@ -2,9 +2,12 @@ package com.github.saphyra.apphub.ci.process.local;
 
 import com.github.saphyra.apphub.ci.utils.ServicePinger;
 import com.github.saphyra.apphub.ci.value.Service;
+import com.google.common.base.Stopwatch;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -15,6 +18,7 @@ public class LocalStartTask implements Runnable {
     @Override
     @SneakyThrows
     public void run() {
+        Stopwatch stopwatch = Stopwatch.createStarted();
         log.info("Starting service {}", service.getName());
 
         new ProcessBuilder("cmd", "/c", "start", "java", "-Xmx512m", "-Dfile.encoding=UTF-8", "-DSPRING_ACTIVE_PROFILE=local", "-jar", service.getLocation())
@@ -25,6 +29,7 @@ public class LocalStartTask implements Runnable {
                 throw new IllegalStateException(service.getName() + " failed to start.", cause);
             });
 
-        log.info("{} successfully started.", service.getName());
+        stopwatch.stop();
+        log.info("{} successfully started in {}s.", service.getName(), (stopwatch.elapsed(TimeUnit.MILLISECONDS) / 1000d));
     }
 }
