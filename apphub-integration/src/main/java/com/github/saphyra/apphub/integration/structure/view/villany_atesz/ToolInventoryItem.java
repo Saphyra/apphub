52e14062081845f319c605c3e7a5bddeb474c184
saphyra <bondabear@gmail.com>
2024-10-06 12:21:15 +0200

    Fix unstable integration test

--- apphub-integration/src/main/java/com/github/saphyra/apphub/integration/structure/view/villany_atesz/ToolInventoryItem.java
+++ apphub-integration/src/main/java/com/github/saphyra/apphub/integration/structure/view/villany_atesz/ToolInventoryItem.java
@@ -19,7 +19,9 @@ public class ToolInventoryItem {
     }
 
     public void editBrand(String newBrand) {
-        WebElementUtils.clearAndFill(webElement.findElement(By.className("villany-atesz-toolbox-inventory-item-brand")), newBrand);
+        for (int i = 0; i < 5 && !getBrand().equals(newBrand); i++) {
+            WebElementUtils.clearAndFill(webElement.findElement(By.className("villany-atesz-toolbox-inventory-item-brand")), newBrand);
+        }
     }
 
     public void editName(String newName) {
