790314a50e648df9b0b765cd9928afa5edbaee89
bondabear@gmail.com <Saphy371321>
2024-12-21 16:12:55 +0100

    ApphubCi - Fix project structure

--- apphub-ci/src/main/java/com/github/saphyra/apphub/ci/process/minikube/preprod/StartPreprodProxyProcess.java
+++ apphub-ci/src/main/java/com/github/saphyra/apphub/ci/process/minikube/preprod/StartPreprodProxyProcess.java
@@ -8,6 +8,7 @@ import com.github.saphyra.apphub.ci.utils.ServicePinger;
 import com.github.saphyra.apphub.ci.value.Constants;
 import com.github.saphyra.apphub.ci.value.PlatformProperties;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -19,9 +20,16 @@ public class StartPreprodProxyProcess {
     private final PortForwardTask portForwardTask;
     private final MinikubePodStartupWaiter minikubePodStartupWaiter;
 
+    @SneakyThrows
     public void startPreprodProxy() {
         minikubePodStartupWaiter.waitForPods(Constants.NAMESPACE_NAME_PREPROD);
         processKiller.killByPort(platformProperties.getMinikubePreprodServerPort());
+
+        Process process = new ProcessBuilder("cmd", "/c", "cd", "apphub-proxy", "&&", "mvn", "clean", "package")
+            .inheritIO()
+            .start();
+
+        process.waitFor();
 
         portForwardTask.portForward(Constants.NAMESPACE_NAME_PREPROD, Constants.SERVICE_NAME_MAIN_GATEWAY, platformProperties.getMinikubePreprodMainGatewayPort(), Constants.SERVICE_PORT);
 
