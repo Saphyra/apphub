a59b7b91f8c602b95bef683f44294a24b0e32003
bondabear@gmail.com <Saphy371321>
2025-02-04 15:37:18 +0100

    Todo elimination

--- apphub-service/apphub-service-elite_base/src/main/java/com/github/saphyra/apphub/service/elite_base/message_processing/dao/last_update/LastUpdateDao.java
+++ apphub-service/apphub-service-elite_base/src/main/java/com/github/saphyra/apphub/service/elite_base/message_processing/dao/last_update/LastUpdateDao.java
@@ -1,18 +1,16 @@
 package com.github.saphyra.apphub.service.elite_base.message_processing.dao.last_update;
 
 import com.github.saphyra.apphub.lib.common_util.CachedDao;
-import com.github.saphyra.apphub.lib.common_util.converter.Converter;
 import com.github.saphyra.apphub.lib.common_util.converter.UuidConverter;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
 @Component
-//TODO unit test
 public class LastUpdateDao extends CachedDao<LastUpdateEntity, LastUpdate, LastUpdateId, LastUpdateRepository> {
     private final UuidConverter uuidConverter;
 
-    LastUpdateDao(Converter<LastUpdateEntity, LastUpdate> converter, LastUpdateRepository repository, UuidConverter uuidConverter, LastUpdateCache cache) {
+    LastUpdateDao(LastUpdateConverter converter, LastUpdateRepository repository, UuidConverter uuidConverter, LastUpdateCache cache) {
         super(converter, repository, false, cache);
         this.uuidConverter = uuidConverter;
     }
