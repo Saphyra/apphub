22ee0e19a45a409d8da42a99510d6f29dd546521
bondabear@gmail.com <Saphy371321>
2025-07-12 14:36:14 +0200

    AdminPanel - PerformanceReporting - Order reports

--- apphub-frontend/src/modules/admin_panel/performance_reporting/PerformanceReportingReports.jsx
+++ apphub-frontend/src/modules/admin_panel/performance_reporting/PerformanceReportingReports.jsx
@@ -5,6 +5,27 @@ import { formatDuration } from "../../../common/js/Utils";
 import Button from "../../../common/component/input/Button";
 import { ADMIN_PANEL_PERFORMANCE_REPORTING_DELETE_REPORTS } from "../../../common/js/dao/endpoints/AdminPanelEndpoints";
 import InputField from "../../../common/component/input/InputField";
+import PreLabeledInputField from "../../../common/component/input/PreLabeledInputField";
+import SelectInput, { SelectOption } from "../../../common/component/input/SelectInput";
+
+const KEY = "key";
+const MINIMUM = "minimum";
+const MAXIMUM = "maximum";
+const AVERAGE = "average";
+const COUNT = "count";
+const ASCENDING = "ascending";
+const DESCENDING = "descending";
+
+const ORDER_BY = {}
+ORDER_BY[KEY] = (a, b) => a.key.localeCompare(b.key);
+ORDER_BY[MINIMUM] = (a, b) => a.min - b.min;
+ORDER_BY[MAXIMUM] = (a, b) => a.max - b.max;
+ORDER_BY[AVERAGE] = (a, b) => a.average - b.average;
+ORDER_BY[COUNT] = (a, b) => a.count - b.count;
+
+const DIRECTION = {};
+DIRECTION[ASCENDING] = value => value;
+DIRECTION[DESCENDING] = value => -1 * value;
 
 const PerformanceReportingReports = ({ reports, localizationHandler, loadReports }) => {
     const getContent = () => {
@@ -27,6 +48,8 @@ const PerformanceReportingReports = ({ reports, localizationHandler, loadReports
 
 const TopicReports = ({ topic, reports, localizationHandler, loadReports }) => {
     const [searchText, setSearchText] = useState("");
+    const [orderBy, setOrderBy] = useState(KEY);
+    const [direction, setDirection] = useState(ASCENDING);
 
     return (
         <fieldset className={"performance-reporting-reports-topic " + topic}>
@@ -44,6 +67,22 @@ const TopicReports = ({ topic, reports, localizationHandler, loadReports }) => {
                     onchangeCallback={setSearchText}
                     value={searchText}
                 />
+
+                <PreLabeledInputField
+                    label={localizationHandler.get("order-by")}
+                    input={<SelectInput
+                        value={orderBy}
+                        options={getOrderByOptions()}
+                        onchangeCallback={setOrderBy}
+                    />
+                    }
+                />
+
+                <SelectInput
+                    value={direction}
+                    options={getDirectionOptions()}
+                    onchangeCallback={setDirection}
+                />
             </legend>
 
             <div className="performance-reproting-reports-topic-reports">
@@ -52,9 +91,23 @@ const TopicReports = ({ topic, reports, localizationHandler, loadReports }) => {
         </fieldset>
     );
 
+    function getDirectionOptions() {
+        return new Stream(Object.keys(DIRECTION))
+            .toMapStream(key => key, key => localizationHandler.get(key))
+            .sorted((a, b) => a.value.localeCompare(b.value))
+            .toList((key, label) => new SelectOption(label, key));
+    }
+
+    function getOrderByOptions() {
+        return new Stream(Object.keys(ORDER_BY))
+            .toMapStream(key => key, key => localizationHandler.get(key))
+            .sorted((a, b) => a.value.localeCompare(b.value))
+            .toList((key, label) => new SelectOption(label, key));
+    }
+
     function getContent() {
         return new Stream(reports)
-            .sorted((a, b) => a.key.localeCompare(b.key))
+            .sorted((a, b) => DIRECTION[direction](ORDER_BY[orderBy](a, b)))
             .filter(report => report.key.toLowerCase().indexOf(searchText.toLowerCase()) > -1)
             .map(report => <TopicReportItem
                 key={report.key}
