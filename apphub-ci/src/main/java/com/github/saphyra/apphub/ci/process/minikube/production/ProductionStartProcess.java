abd4aedbef934f8a63d6aa5e442a3a82d141b1c7
saphyra <bondabear@gmail.com>
2025-07-13 09:14:09 +0200

    Release 1.19.1
    Fix: ApphubCI scales down prod when starting VM

--- apphub-ci/src/main/java/com/github/saphyra/apphub/ci/process/minikube/production/ProductionStartProcess.java
+++ apphub-ci/src/main/java/com/github/saphyra/apphub/ci/process/minikube/production/ProductionStartProcess.java
@@ -1,5 +1,6 @@
 package com.github.saphyra.apphub.ci.process.minikube.production;
 
+import com.github.saphyra.apphub.ci.process.minikube.MinikubeScaleProcess;
 import com.github.saphyra.apphub.ci.process.minikube.MinikubeServiceDeployer;
 import com.github.saphyra.apphub.ci.process.minikube.MinikubeStartProcess;
 import com.github.saphyra.apphub.ci.value.Constants;
@@ -12,9 +13,12 @@ public class ProductionStartProcess {
     private final MinikubeStartProcess minikubeStartProcess;
     private final MinikubeServiceDeployer minikubeServiceDeployer;
     private final StartProductionProxyProcess startProductionProxyProcess;
+    private final MinikubeScaleProcess minikubeScaleProcess;
 
     public void startServer() {
         minikubeStartProcess.startMinikube();
+
+        minikubeScaleProcess.scale(Constants.NAMESPACE_NAME_PRODUCTION, 0);
 
         minikubeServiceDeployer.deploy(Constants.NAMESPACE_NAME_PRODUCTION, Constants.DIR_NAME_PRODUCTION, 60);
 
