d7da101ee74803a7acc1f0952fed010160335ae0
Saphyra <bondabear@gmail.com>
2025-11-15 08:20:26 +0100

    AdminPanel - PerformanceReporting - Hide keys

--- apphub-frontend/src/modules/admin_panel/performance_reporting/PerformanceReporting.jsx
+++ apphub-frontend/src/modules/admin_panel/performance_reporting/PerformanceReporting.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import localizationData from "./performance_reporting_localization.json";
 import LocalizationHandler from "../../../common/js/LocalizationHandler";
 import Header from "../../../common/component/Header";
