43c5070d9a387a8d09de72c60608e2aebe170efa
Saphyra <bondabear@gmail.com>
2026-06-12 11:16:10 +0200

    Storage - Remove PSQL dependencies

--- apphub-service/apphub-service-platform/apphub-service-platform-storage/src/main/java/com/github/saphyra/apphub/service/platform/storage/config/StorageBeanConfig.java
+++ apphub-service/apphub-service-platform/apphub-service-platform-storage/src/main/java/com/github/saphyra/apphub/service/platform/storage/config/StorageBeanConfig.java
@@ -6,10 +6,8 @@ import com.github.saphyra.apphub.service.platform.storage.StorageApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EnableJpaRepositories(basePackageClasses = StorageApplication.class)
 @EntityScan(basePackageClasses = StorageApplication.class)
 public class StorageBeanConfig {
     @Bean
