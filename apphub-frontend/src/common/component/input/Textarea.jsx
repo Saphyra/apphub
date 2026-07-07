d3f6b5b51bb560008780dc3ef7f82c82a6cd895a
Saphyra <bondabear@gmail.com>
2026-07-07 15:21:53 +0200

    Textarea natively supports tab->space and auto-resize

--- apphub-frontend/src/common/component/input/Textarea.jsx
+++ apphub-frontend/src/common/component/input/Textarea.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { replaceTabsWithSpaces } from "./TabReplacer";
 
 const Textarea = ({
     id,
@@ -9,21 +10,39 @@ const Textarea = ({
     disabled = false,
     onKeyUpCallback = () => { },
     onKeyDownCallback = () => { },
-    reference = () =>{},
+    reference = () => { },
     style,
-    rows
+    rows,
+    spacesInsteadOfTab = false,
+    autoResize = false
 }) => {
+    const onKeyDown = spacesInsteadOfTab ?
+        (e) => {
+            replaceTabsWithSpaces(e);
+            onKeyDownCallback(e);
+        }
+        : onKeyDownCallback;
+
+    const onKeyUp = autoResize ?
+        (e) => {
+            e.target.style.height = "auto";
+            e.target.style.height = e.target.scrollHeight + 6 + "px";
+
+            onKeyUpCallback(e);
+        }
+        : onKeyUpCallback;
+
     return (
         <textarea
             ref={r => reference(r)}
             id={id}
-            className={className}
+            className={className + (autoResize ? " resizable" : "")}
             onChange={(e) => onchangeCallback(e.target.value)}
             placeholder={placeholder}
             value={value}
             disabled={disabled}
-            onKeyUp={onKeyUpCallback}
-            onKeyDown={onKeyDownCallback}
+            onKeyUp={onKeyUp}
+            onKeyDown={onKeyDown}
             style={style}
             rows={rows}
         />
